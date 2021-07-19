925a2a5bde57d89d3f9449faa676a79c32128813
Your Name <you@example.com>
2021-07-19 11:55:29 +0700

    ANH update code 11h56 - 19/7

--- anime-main/User/blog.php
+++ anime-main/User/blog.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="Anime, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Anime | Template</title>
+    <title>Anime | Blog</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -24,7 +24,78 @@
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
+<style>
+    * {
+  box-sizing: border-box;
+}
 
+/* Create two equal columns that floats next to each other */
+.columnblog1 {
+  float: left;
+  width: 25%;
+  padding-right: 10px;
+  height: 150px; /* Should be removed. Only for demonstration */
+}
+.columnblog2 {
+  float: left;
+  width: 75%;
+  padding: 10px;
+  height: 250px; /* Should be removed. Only for demonstration */
+}
+/* Clear floats after the columns */
+.rowblog:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+.columncard {
+  float: left;
+  width: 25%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+  
+}
+
+.cardcard {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.containercard {
+  padding: 0 16px;
+}
+
+.containercard::after, .rowcard::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.titlecard {
+  color: grey;
+}
+
+.buttoncard {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.buttoncard:hover {
+}
+.container {
+  position: relative;
+  font-family: Arial;
+}
+.containerso {
+  position: relative;
+  font-family: Arial;
+}
+</style>
 <body>
     <!-- Page Preloder -->
     <div id="preloder">
@@ -82,129 +153,56 @@
         </div>
     </header>
     <!-- Header End -->
-
-    <!-- Normal Breadcrumb Begin -->
-    <section class="normal-breadcrumb set-bg" data-setbg="">
+        <div class="breadcrumb-option">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="normal__breadcrumb__text">
-                        <h2>Blog</h2>
-
+                <div class="col-lg-12">
+                    <div class="breadcrumb__links">
+                        <a href="./index.php"><i class="fa fa-home"></i>TRANG CHỦ</a>
+                        <span>BLOG</span>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- Normal Breadcrumb End -->
-
+    </div>
     <!-- Blog Section Begin -->
-    <section class="blog spad">
+    <section class="product spad">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="blog__item set-bg" data-setbg="img/blog/blog-1.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="product__page__title">
+                            <div class="row">
+                                <div class="col-lg-10 col-md-10 col-sm-6">
+                                    <div class="section-title">
+                                        <h4>BLOG</h4>
+                                    </div>
+                                </div>
+                                <div class="col-lg-2 col-md-2 col-sm-6">
+
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-4.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Bok no Hero Academia Season 4 – 18</a></h4>
-                                </div>
+                    <?php
+                        require_once("Connection.php");
+                        $query=mysqli_query($conn,"select * from blog ORDER BY id DESC LIMIT 0, 10");
+                        while($row = mysqli_fetch_array($query)){
+                        ?>
+                    <div class="rowblog">
+                        <div class="columnblog1" style="">
+                            <div class="containerso">
+                                <a href="blog-details.php?id=<?php echo $row['id']; ?>"><img src="<?php echo $row['anh'];?>" alt="Jane" style="width:100%;height:250px;"></a>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-5.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="blog__item set-bg" data-setbg="img/blog/blog-7.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Housekishou Richard shi no Nazo Kantei Season 08 - 20</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-10.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-11.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Building a Better LiA Drilling Down</a></h4>
-                                </div>
-                            </div>
+                        <div class="columnblog2" style="background-color:#FFEC8B;">
+                            <a href="blog-details.php?id=<?php echo $row['id']; ?>"><h2 style="color:black;padding-top:5px;padding-bottom:5px;"><?php echo $row['tenblog'];?></h2></a>
+                            <p><?php echo $row['ngaydang'];?></p>
+                            <p><?php echo $row['motaindex'];?></p>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-2.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-3.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Building a Better LiA Drilling Down</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="blog__item set-bg" data-setbg="img/blog/blog-6.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-8.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Bok no Hero Academia Season 4 – 18</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="blog__item small__item set-bg" data-setbg="img/blog/blog-9.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="blog__item set-bg" data-setbg="img/blog/blog-12.jpg">
-                                <div class="blog__item__text">
-                                    <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                    <h4><a href="#">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <br>
+                    <?php
+                        }
+                    ?>
                 </div>
             </div>
         </div>
@@ -213,35 +211,60 @@
 
     <!-- Footer Section Begin -->
     <footer class="footer">
-        <div class="page-up">
-            <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="footer__logo">
-                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="footer__nav">
-                        <ul>
-                            <li class="active"><a href="./index.html">Homepage</a></li>
-                            <li><a href="./categories.html">Categories</a></li>
-                            <li><a href="./blog.html">Our Blog</a></li>
-                            <li><a href="#">Contacts</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                      Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+    <div class="page-up">
+        <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                    <h2>Liên hệ Admin</h2>
+                    <br>
+                    <p> Tổng hợp tất cả các bộ anime trên trang được tổng hợp từ Internet và re-up từ các Fansub.</p>
+                    <p><i class="fab fa-facebook-square">FACEBOOK:</i> Tuan Anh </p>
+                    <p><i class="fab fa-facebook-messenger">MESSENGER:</i> Huy Lee </p>
+                    <p><i class="fas fa-envelope">EMAIL:</i> wibuteam@gmail.com  </p>
 
-                  </div>
+            </div>
+            <div class="col-lg-6">
+                <h2>Project Anime</h2>
+                <br>
+                <div class="footer-col">
+                <p>Liên hệ công ty thiết kế</p>
+                <p>Giám đốc kiêm trưởng dự án : Tuan Anh</p>
+                <p>Hotline hỗ trợ quảng cáo: 0942 86 11 33</p>
+                <p>Email: wibuhue@gmail.vn</p>
+                <p>Address: Tp huế,tỉnh Thừa thiên Huế</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                  <h2>Thông Tin</h2>
+                  <BR>
+                  <p>Xem tốt nhất t với màn hình độ phân giải 1280x720px trở lên và sử dụng trình duyệt Mozilla Firefox, Chrome & Cốc Cốc.</p>
+                  <p>Bản quyền © 2021 Thiết kế bởi công ty WibuVN.Được quyền bảo lưu và đăng kí bản quyền</p>
+
               </div>
           </div>
-      </footer>
+         <div class="row">
+             <div class="col-lg-3">
+                 
+             </div>
+             <div class="col-lg-6">
+                 <BR>
+        <div class="footer__nav">
+                    <ul>
+                        <li class="active"><a href="index.html">TRANG CHỦ</a></li>
+                        <li><a href="categories.html">THỂ LOẠI</a></li>
+                        <li><a href="theongay.html">TOP ANIME</a></li>
+                        <li><a href="#">LIÊN HỆ</a></li>
+                        <li><a href="blog.php">BLOG</a></li>
+                    </ul>
+                </div>
+             </div>
+              <div class="col-lg-3">
+        
+              </div>
+      </div>
+  </footer>
       <!-- Footer Section End -->
 
       <!-- Search model Begin -->
