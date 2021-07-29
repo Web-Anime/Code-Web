811157b3fd925a43e14737291eb000c5391c076a
Your Name <you@example.com>
2021-07-29 17:36:13 +0700

    KHAI update Login 5h33 - 29/7

--- anime-main/Login/XulyRegiter.php
+++ anime-main/Login/XulyRegiter.php
@@ -1,58 +1,44 @@
 <?php
-		require_once("Cn.php");
+		require_once("Connection.php");
 		if (isset($_POST["btn_regiter"])) {
   			$username = $_POST["username"];
   			$password = $_POST["password"];
                         $password_repeat = $_POST['password_repeat'];
- 			$fullname = $_POST["fullname"];
-                        $diachi = $_POST['diachi'];
-  			$email = $_POST["email"];
-                        $sodienthoai = $_POST['sodienthoai'];
-                        $gioitinh = $_POST['gioitinh'];
-			  if ($username == "" || $password == "" || $password_repeat == "" || $fullname == ""||$diachi == "" || $email == "" || $sodienthoai == "" || $gioitinh == "") {
-				   header('location:ThieuThongTin.php');       
+			  if ($username == "" || $password == "" || $password_repeat == "") {
+				   echo "<script>alert('Username hoặc Password ko đuwocj bỏ trống');</script>";
+                                    require_once 'signup.php';
                         }else{
   					if ($password == $password_repeat){
   					$sql="select * from khachhang where username='$username'";
 					$kt=mysqli_query($conn, $sql);
 					if(mysqli_num_rows($kt)  > 0){
-						header('location:TrungNhau.php');
+						echo "<script>alert('Username đã tồn tại...!');</script>";
+                                                require_once 'signup.php';
 					}else{
-						
+		
 	    				$sql = "INSERT INTO khachhang(
-	    					HOTEN,
-	    					USERNAME,
-	    					PASSWORD,
-                                                DIACHI,
-						EMAIL,
-                                                SDT,
-                                                GIOITINH
+
+	    					username,
+	    					password
+
 	    					) VALUES (
-	    					'$fullname',
 	    					'$username',
-						'$password',
-	    					'$diachi',
-                                                '$email',
-                                                '$sodienthoai',
-                                                '$gioitinh'
+						'$password'
+
 	    					)";
 					 
    						mysqli_query($conn,$sql);
-				   		header('location:ThanhCong.php');
+				   		header('location: login.php');
                                           
 					}
 									    
 					
 			  }else{
-                              header('location:TrungNhau.php');
+                              echo "<script>alert('Password ko trùng khớp...!');</script>";
+                              require_once 'signup.php';
                           }
 	}
                 }
 	?>
-   <br>
- <a href="FormLogout.php">Trở về đăng kí  </a>
- <BR>
-
- <a href="http://localhost:8080/CothaoBai5/Bai2/login.php">Dang nhập</a>
  
 
