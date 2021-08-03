91fd686ca1cb0c54f89986956cf2380704f34a0f
tuan <tuanbosstrum@gmail.com>
2021-08-03 16:36:28 +0700

    Tuấn Updatecode

--- anime-main/blog.php
+++ anime-main/blog.php
@@ -3,7 +3,7 @@
 
 <head>
     <LINK REL="SHORTCUT ICON"  HREF="View/Client/WIBU.png">
-    <title>Anime | Blogger</title>
+    <title>Anime | Blog</title>
     <?php include 'View/Client/Layout/Link.php'; ?>
 </head>
 <style>
